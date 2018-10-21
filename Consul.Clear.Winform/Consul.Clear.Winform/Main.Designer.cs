613201013e03b18063b90c26cc2af3b580b3a4b4
汤波 <478185912@qq.com>
2018-10-21 22:37:46 +0800

    上传客户端删除consul服务的源码和安装程序

---
+++ Consul.Clear.Winform/Consul.Clear.Winform/Main.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Consul.Clear.Winform
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_ip = new System.Windows.Forms.TextBox();
+            this.btn_query = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tb_log = new System.Windows.Forms.TextBox();
+            this.tb_port = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Select = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Node = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ServiceID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ServiceName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btn_select = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "服务地址：";
+            // 
+            // tb_ip
+            // 
+            this.tb_ip.Location = new System.Drawing.Point(97, 6);
+            this.tb_ip.Name = "tb_ip";
+            this.tb_ip.Size = new System.Drawing.Size(148, 26);
+            this.tb_ip.TabIndex = 1;
+            // 
+            // btn_query
+            // 
+            this.btn_query.Location = new System.Drawing.Point(409, 6);
+            this.btn_query.Name = "btn_query";
+            this.btn_query.Size = new System.Drawing.Size(75, 26);
+            this.btn_query.TabIndex = 2;
+            this.btn_query.Text = "查询";
+            this.btn_query.UseVisualStyleBackColor = true;
+            this.btn_query.Click += new System.EventHandler(this.btn_query_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(571, 6);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(75, 26);
+            this.btn_clear.TabIndex = 3;
+            this.btn_clear.Text = "清理";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Select,
+            this.Node,
+            this.ServiceID,
+            this.ServiceName});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 38);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(634, 611);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // tb_log
+            // 
+            this.tb_log.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tb_log.Location = new System.Drawing.Point(652, 6);
+            this.tb_log.Multiline = true;
+            this.tb_log.Name = "tb_log";
+            this.tb_log.ReadOnly = true;
+            this.tb_log.Size = new System.Drawing.Size(270, 643);
+            this.tb_log.TabIndex = 5;
+            // 
+            // tb_port
+            // 
+            this.tb_port.Location = new System.Drawing.Point(322, 6);
+            this.tb_port.Name = "tb_port";
+            this.tb_port.Size = new System.Drawing.Size(81, 26);
+            this.tb_port.TabIndex = 7;
+            this.tb_port.Text = "8500";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(251, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "端口号：";
+            // 
+            // Select
+            // 
+            this.Select.HeaderText = "选中";
+            this.Select.Name = "Select";
+            this.Select.TrueValue = "";
+            this.Select.Width = 43;
+            // 
+            // Node
+            // 
+            this.Node.DataPropertyName = "Node";
+            this.Node.HeaderText = "节点";
+            this.Node.Name = "Node";
+            this.Node.Width = 62;
+            // 
+            // ServiceID
+            // 
+            this.ServiceID.DataPropertyName = "ServiceID";
+            this.ServiceID.HeaderText = "服务ID";
+            this.ServiceID.Name = "ServiceID";
+            this.ServiceID.Width = 77;
+            // 
+            // ServiceName
+            // 
+            this.ServiceName.DataPropertyName = "ServiceName";
+            this.ServiceName.HeaderText = "服务名称";
+            this.ServiceName.Name = "ServiceName";
+            this.ServiceName.Width = 90;
+            // 
+            // btn_select
+            // 
+            this.btn_select.Location = new System.Drawing.Point(490, 6);
+            this.btn_select.Name = "btn_select";
+            this.btn_select.Size = new System.Drawing.Size(75, 26);
+            this.btn_select.TabIndex = 8;
+            this.btn_select.Tag = "0";
+            this.btn_select.Text = "全不选";
+            this.btn_select.UseVisualStyleBackColor = true;
+            this.btn_select.Click += new System.EventHandler(this.btn_select_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(934, 661);
+            this.Controls.Add(this.btn_select);
+            this.Controls.Add(this.tb_port);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb_log);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_query);
+            this.Controls.Add(this.tb_ip);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.ShowIcon = false;
+            this.Text = "Consul服务移除工具";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_ip;
+        private System.Windows.Forms.Button btn_query;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox tb_log;
+        private System.Windows.Forms.TextBox tb_port;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Select;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Node;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ServiceID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ServiceName;
+        private System.Windows.Forms.Button btn_select;
+    }
+}
---
+++ Consul.Clear.Winform/Consul.Clear.Winform/Main.cs
@@ -0,0 +1,196 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Consul.Clear.Winform
+{
+    public partial class Main : Form
+    {
+        private const string query_addr = "/v1/health/state/critical";
+        private const string clear_addr = "/v1/agent/service/deregister/";
+        private string input_addr = "";
+
+        public Main()
+        {
+            InitializeComponent();
+            dataGridView1.AutoGenerateColumns = false;
+        }
+
+        private async void btn_query_Click(object sender, EventArgs e)
+        {
+            string ip = tb_ip.Text;
+            string port = tb_port.Text;
+            if (string.IsNullOrEmpty(ip))
+            {
+                MessageBox.Show("请输入服务地址");
+                return;
+            }
+
+            input_addr = string.IsNullOrEmpty(port) ? ip : ip + ":" + port;
+            JArray ja = await getService();
+
+            if (ja == null)
+            {
+                return;
+            }
+
+            await Task.Factory.StartNew(() => SetData(ja));
+        }
+
+        private async Task<JArray> getService()
+        {
+            if (string.IsNullOrEmpty(input_addr))
+            {
+                return null;
+            }
+
+            //建立 HttpClient
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync(string.Format("http://{0}{1}", input_addr, query_addr));
+
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        StringBuilder sb = new StringBuilder();
+                        sb.Append("请求返回失败\r\n");
+                        sb.Append(" 返回码:" + response.StatusCode + "\r\n");
+                        sb.Append(" 返回信息:" + response.Content + "\r\n");
+                        AppendLog(sb.ToString());
+                        return null;
+                    }
+
+                    string result = await response.Content.ReadAsStringAsync();
+                    JArray ja = (JArray)JsonConvert.DeserializeObject(result);
+                    AppendLog("获取数据成功，获取到 " + ja.Count + " 条服务");
+                    return ja;
+                }
+                catch (Exception ex)
+                {
+                    AppendLog(ex.Message);
+                    return null;
+                }
+            }
+        }
+
+        private void btn_select_Click(object sender, EventArgs e)
+        {
+            bool state = Convert.ToInt16(btn_select.Tag) == 1;
+            foreach (DataGridViewRow item in dataGridView1.Rows)
+            {
+                item.Cells[0].Value = state;
+            }
+            btn_select.Tag = state ? 0 : 1;
+            btn_select.Text = state ? "全不选" : "全选";
+        }
+
+        private async void btn_clear_Click(object sender, EventArgs e)
+        {
+            List<string> list = new List<string>();
+
+            foreach (DataGridViewRow item in dataGridView1.Rows)
+            {
+                bool isChecked = Convert.ToBoolean(item.Cells[0].EditedFormattedValue);
+                if (isChecked)
+                {
+                    list.Add(item.Cells["ServiceID"].Value.ToString());
+                }
+            }
+
+            if (list.Count == 0)
+            {
+                MessageBox.Show("您当前没有选中的服务");
+                return;
+            }
+
+            if (MessageBox.Show("您当前选中了 " + list.Count + " 条服务\r\n是否确认删除", "", MessageBoxButtons.OKCancel) != DialogResult.OK) { return; }
+
+            List<string> removed = await delService(list);
+
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                string serviceID = dataGridView1.Rows[i].Cells["ServiceID"].Value.ToString();
+                if (!removed.Contains(serviceID)) { continue; }
+                dataGridView1.Rows.RemoveAt(i);
+                i--;
+            }
+
+            AppendLog("删除成功，已删除 " + removed.Count + " 条服务");
+        }
+
+        private async Task<List<string>> delService(List<string> ids)
+        {
+            List<string> list = new List<string>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                foreach (string id in ids)
+                {
+                    try
+                    {
+                        var response = await client.PutAsync(string.Format("http://{0}{1}{2}", input_addr, clear_addr, id), null);
+
+                        if (!response.IsSuccessStatusCode)
+                        {
+                            StringBuilder sb = new StringBuilder();
+                            sb.Append("请求返回失败\r\n");
+                            sb.Append(" 返回码:" + response.StatusCode + "\r\n");
+                            sb.Append(" 返回信息:" + response.Content + "\r\n");
+                            AppendLog(sb.ToString());
+                            continue;
+                        }
+
+                        AppendLog("删除成功，服务ID： " + id);
+                        list.Add(id);
+                    }
+                    catch (Exception ex)
+                    {
+                        AppendLog(ex.Message);
+                    }
+                }
+            }
+
+            return list;
+        }
+
+        #region 操作UI
+        private delegate void appendLog(string log);
+
+        private void AppendLog(string log)
+        {
+            if (tb_log.InvokeRequired)
+            {
+                BeginInvoke(new appendLog(AppendLog), log);
+            }
+            else
+            {
+                tb_log.AppendText(log + "\r\n");
+            }
+        }
+
+        private delegate void setData(JArray array);
+
+        private void SetData(JArray array)
+        {
+            if (dataGridView1.InvokeRequired)
+            {
+                BeginInvoke(new setData(SetData), array);
+            }
+            else
+            {
+                dataGridView1.DataSource = array;
+                foreach (DataGridViewRow item in dataGridView1.Rows)
+                {
+                    item.Cells[0].Value = true;
+                }
+            }
+        }
+        #endregion
+    }
+}
